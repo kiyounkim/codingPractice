3df89ff56a8b785f30d4fa7133dc81f0c1d3eb1a
kiyounkim <112357059+kiyounkim@users.noreply.github.com>
2023-06-05 01:10:31 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -0,0 +1,12 @@
+    #include <iostream>
+
+    using namespace std;
+
+    int main() {
+        int A,B,V;
+        cin >> A >> B >> V;
+        int day = 1;
+        day += (V-A)/(A-B);
+        if((V-A)%(A-B) != 0) day++;
+        cout << day << endl;
+    }
