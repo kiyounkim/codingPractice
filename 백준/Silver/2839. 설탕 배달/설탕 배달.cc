f4f484e6070cda8a9462e68005ad3c0d17ab23b1
kiyounkim <112357059+kiyounkim@users.noreply.github.com>
2023-08-11 12:31:45 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -1,20 +1,27 @@
 #include <iostream>
-#include <string>
+
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
-    int count=0;
-    while(n>=0){
-        if(n%5==0){
-            count+=n/5;
-            cout << count;
-            return 0;
-        }
-        n-=3;
-        count++;
-    }
-    cout << -1;
-    return 0;
+int main()
+{
+	ios::sync_with_stdio(false); 
+    cin.tie(nullptr);
+	int n;
+	cin >> n;
+	int count = 0;
+	while (n >= 0)
+	{
+		if (n % 5 == 0)
+		{
+			count += n / 5;
+			cout << count;
+			return 0;
+		}
+		else
+		{
+			n -= 3;
+			count++;
+		}
+	}
+	cout << -1;
 }
