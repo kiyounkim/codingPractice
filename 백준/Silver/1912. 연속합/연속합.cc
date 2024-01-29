2f57d12f6a27d8067597d2d32084260b414c9011
kiyounkim <112357059+kiyounkim@users.noreply.github.com>
2024-01-29 17:46:45 +0900

    [Silver II] Title: 연속합, Time: 28 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -2,24 +2,26 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+
 int main() {
     int n;
-    cin>>n;
-    vector<int> num;
-    vector<int> dp;
-    for(int i=0; i<n; i++) {
-        int temp;
-        cin>>temp;
-        num.push_back(temp);
-        dp.push_back(temp);
+    cin >> n;
+    vector<int> num(n);
+    vector<int> dp(n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> num[i];
+        dp[i] = num[i];
     }
-    int ans=dp[0];
-    for(int i=1; i<n; i++) {
-        dp[i]=max(dp[i], dp[i-1]+num[i]);
-        if(dp[i]>ans) {
-            ans=dp[i];
+
+    int ans = dp[0];
+    for(int i = 1; i < n; i++) {
+        dp[i] = max(dp[i], dp[i - 1] + num[i]);
+        if(dp[i] > ans) {
+            ans = dp[i];
         }
     }
-    cout<<ans<<endl;
+
+    cout << ans << endl;
     return 0;
 }
