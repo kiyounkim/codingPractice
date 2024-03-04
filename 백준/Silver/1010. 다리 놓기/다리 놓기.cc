5373308e0efc57c63dc6b31051785fc489bd2219
kiyounkim <112357059+kiyounkim@users.noreply.github.com>
2024-03-05 08:17:17 +0900

    [Silver V] Title: 다리 놓기, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.cc
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -1,31 +1,20 @@
 #include <iostream>
-#include <vector>
+
 using namespace std;
 
-int nCr(int n, int r) {
-    vector<vector<int>> dp(n + 1, vector<int>(r + 1, 0));
-    for (int i = 0; i <= n; i++) {
-        dp[i][0] = 1;
-    }
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= min(i, r); j++) {
-            if (j == i) {
-                dp[i][j] = 1;
-            } else {
-                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-            }
+int main(){
+    int T;
+    cin >> T;
+    for(int i=0;i<T;i++){
+        int samples,totalItems;
+        cin >> samples >> totalItems;
+        long long ans = 1;
+        int r = 1;
+        for(int j=totalItems;j>totalItems-samples;j--){
+            ans *=j;
+            ans /= r;
+            r++;
         }
+        cout << ans << endl;
     }
-    return dp[n][r];
-}
-
-int main() {
-    int n;
-    cin >> n;
-    for(int i=0;i<n;i++){
-        int a,b;
-        cin >> a >> b;
-        cout << nCr(b,a) << endl;
-    }
-    return 0;
 }
