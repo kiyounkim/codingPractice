fe27bb13480321e3d2f257731b29ac618c7dce73
kiyounkim <112357059+kiyounkim@users.noreply.github.com>
2024-03-04 12:43:14 +0900

    [Bronze IV] Title: 평균 점수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int sum=0;
+    for(int i=0;i<5;i++){
+        cin>>n;
+        if(n<40)
+            n = 40;
+        sum+=n;
+    }
+    cout<<sum/5;
+}
