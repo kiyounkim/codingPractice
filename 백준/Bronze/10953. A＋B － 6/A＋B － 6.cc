eca2bb5033dcbe580101d95d7ec94b67a7b056c8
kiyounkim <112357059+kiyounkim@users.noreply.github.com>
2024-03-09 20:31:21 +0900

    [Bronze III] Title: A+B - 6, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        int n,m;
+        char c;
+        cin >> n >> c >> m;
+        cout << n+m << endl;
+    }
+    return 0;
+}
