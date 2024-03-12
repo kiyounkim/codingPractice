2457ddcfdc509e966ca61855b62f14b8d58291d5
kiyounkim <112357059+kiyounkim@users.noreply.github.com>
2024-03-12 10:38:31 +0900

    [Silver V] Title: 뒤집기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.cc
+++ 백준/Silver/1439. 뒤집기/뒤집기.cc
@@ -1,20 +1,17 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
+
 using namespace std;
 
 int main() {
     string s;
+    int result=0;
     cin >> s;
-    int count0 = 0, count1 = 0;
-    if (!s.empty()) {
-        if (s[0] == '0') count0++;
-        else count1++;
-    }
-    for (size_t i = 1; i < s.length(); ++i) {
-        if (s[i] == '0' && s[i-1] == '1') count0++;
-        else if (s[i] == '1' && s[i-1] == '0') count1++;
-    }
-    cout << min(count0, count1) << endl;
-    return 0;
+
+    for(int i=0;i<s.length();i++) 
+        if(s[i]!=s[i+1]) 
+            result++;
+
+    if(!result) 
+        cout << result;
+    else cout << result/2;
 }
