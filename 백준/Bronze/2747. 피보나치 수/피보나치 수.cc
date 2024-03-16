2c2eadd1cb1848be77d83d5056525784fadfec1b
kiyounkim <112357059+kiyounkim@users.noreply.github.com>
2024-03-16 17:27:56 +0900

    [Bronze II] Title: 피보나치 수, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[1000000];
+int fib(int n){
+	if(n == 0) return 0;
+	if(n == 1) return 1;
+	if(arr[n] != 0) return arr[n];
+	arr[n] = fib(n-1) + fib(n-2);
+	return arr[n];
+}
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	cout << fib(n) << endl;
+}
