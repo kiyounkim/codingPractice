081fd8a354cac90a250463083b4b84f53ad01f66
kiyounkim <112357059+kiyounkim@users.noreply.github.com>
2024-04-06 21:11:23 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -1,30 +1,21 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
-
-int a[41] = {0,1};
-
-void fibonacci(int n) {
-	if(n==0) return;
-	for(int i=2;i<=n;i++){
-		a[i] = a[i-1] + a[i-2];
-	}
-}
-
-int main() {
-	ios::sync_with_stdio(false); 
-    cin.tie(nullptr);
-	int n;
-	cin >> n;
-	for(int i=0;i<n;i++){
-		int num;
-		cin >> num;
-		if(num == 0)
-			cout << "1 0" << endl;
-		else {
-			fibonacci(num);
-			cout << a[num-1] << " " << a[num] << endl;
-		}
-	}
-	return 0;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    int t;
+    vector<pair<int, int>> v;
+    v.push_back({1, 0});
+    v.push_back({0, 1});
+    for(int i=2;i<=40;i++){
+        v.push_back({v[i-1].first + v[i-2].first, v[i-1].second + v[i-2].second});
+    }
+    for(int i=0;i<n;i++){
+        cin >> t;
+        cout << v[t].first << " " << v[t].second << "\n";
+    }
+    return 0;
 }
