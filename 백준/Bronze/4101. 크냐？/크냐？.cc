2a9a10c5b2943239c22ea4f9a3ee71ff8199442d
kiyounkim <112357059+kiyounkim@users.noreply.github.com>
2024-04-13 23:34:40 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int a,b;
+    while(1){
+        cin >> a >> b;
+        if(a == 0 && b == 0) break;
+        if(a<=b) cout << "No" << "\n";
+        else cout << "Yes" << "\n";
+    }
+    return 0;
+}
