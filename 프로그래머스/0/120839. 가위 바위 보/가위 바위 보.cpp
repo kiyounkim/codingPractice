b9e5c9d41d7bd5800efe86e49f1a728a8eae5e91
kiyounkim <112357059+kiyounkim@users.noreply.github.com>
2024-05-27 21:53:34 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.cpp
@@ -0,0 +1,21 @@
+#include <string>
+
+using namespace std;
+
+string solution(string rsp) {
+    string answer = "";
+    for(int i = 0; i < rsp.length(); i++) {
+        switch(rsp[i]) {
+            case '2' :
+                answer += "0";
+                break;
+            case '0' :
+                answer += "5";
+                break;
+            case '5' :
+                answer += "2";
+                break;
+        }
+    }
+    return answer;
+}
